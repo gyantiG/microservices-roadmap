e1c2b6482a2f622499156f88f6de320a6e7aa112
gyantiG <gyantigupta2015@gmail.com>
2026-05-18 21:18:20 +0530

    first commit

---
+++ microserviceday1_user_service/src/main/java/com/microservice/day1_user_service/controller/HelloController.java
@@ -0,0 +1,14 @@
+package com.microservice.day1_user_service.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+	
+	@GetMapping("/hello")
+	public String getHello() {
+		return "hi";	
+	}
+
+}
---
+++ microserviceday1_user_service/src/test/java/com/example/microserviceday1_user_service/Microserviceday1UserServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.microserviceday1_user_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Microserviceday1UserServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
