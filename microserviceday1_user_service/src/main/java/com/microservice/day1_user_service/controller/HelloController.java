86c8edf8862e08b50f5c256f7afa288693e3ed8f
gyantiG <gyantigupta2015@gmail.com>
2026-05-19 06:57:05 +0530

    day1 second commit

--- microserviceday1_user_service/src/main/java/com/microservice/day1_user_service/controller/HelloController.java
+++ microserviceday1_user_service/src/main/java/com/microservice/day1_user_service/controller/HelloController.java
@@ -1,14 +1,35 @@
 package com.microservice.day1_user_service.controller;
 
+import java.util.List;
+
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/day1")
 public class HelloController {
 	
 	@GetMapping("/hello")
 	public String getHello() {
 		return "hi";	
+	}
+	
+	// task 1
+	@GetMapping("/{name}")
+	public String getName(@PathVariable String name) {
+		return "Your name is "+name;
+	}
+	
+	@GetMapping("/city")
+	public String getYourCity() {
+		return "your city is Pune";
+	}
+	
+	@GetMapping("/course")
+	public List<String> getYourCourse() {
+		return List.of("BCA","MCA","B-tech");
 	}
 
 }
