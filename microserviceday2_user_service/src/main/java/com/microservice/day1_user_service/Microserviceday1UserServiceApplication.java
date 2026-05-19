901c775acc67fb3ffb1072576f20f379689f1bcc
gyantiG <gyantigupta2015@gmail.com>
2026-05-19 06:59:33 +0530

    Day 2 first commit

---
+++ microserviceday2_user_service/src/main/java/com/microservice/day1_user_service/Microserviceday1UserServiceApplication.java
@@ -0,0 +1,13 @@
+package com.microservice.day1_user_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Microserviceday1UserServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Microserviceday1UserServiceApplication.class, args);
+	}
+
+}
